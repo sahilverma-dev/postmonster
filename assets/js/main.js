4414f50c83de9d2e093cb322363222a20a146729
Sahil Verma <sahilverma.wevdev@gmail.com>
2021-09-30 02:28:16 +0000

    Adding more functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,13 @@
-// utility classes
+// important functions
+const grabEle = (e) => document.querySelector(e);
+const grabAll = (e) => document.querySelectorAll(e);
 const validateUrl = (URL) => {
   return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(
     URL
   );
 };
-
-const getElementFromString = (string) => {
-  let div = document.createElement("div");
-  div.innerHTML = string;
-  return div.firstElementChild;
-};
-
 // show alert box
-const alertBox = document.querySelector(".alert-box");
+const alertBox = grabEle(".alert-box");
 const showAlert = (msg) => {
   alertBox.classList.add("active");
   alertBox.innerText = msg;
@@ -21,133 +16,106 @@ const showAlert = (msg) => {
     alertBox.innerText = "";
   }, 5000);
 };
-
+// grabbing dom elements
+const form = grabEle("form");
+const url = grabEle(".url");
+const preview = grabEle("#responsePrism");
+const addParamBtn = grabEle(".add-params-btn");
+const addHeaderBtn = grabEle(".add-header-btn");
+const paramsContainer = grabEle(".key-params-container");
+const headerContainer = grabEle(".header-params-container");
+let paramIndex = 1;
+let headerIndex = 1;
+let removeParam = [];
+let removeHeader = [];
+let keyParam = [];
+let valueParam = [];
+let headerKey = [grabEle(`#header-key-1`)];
+let headerValue = [grabEle(`#header-value-1`)];
+let headerData = [];
 // adding params
-const paramsCont = document.querySelector(".key-params");
-let paramsConter = 1;
-const addParamBtn = document.querySelector(".add-params-btn");
 addParamBtn.onclick = (e) => {
-  let string = `
+  paramIndex++;
+  const paramEle = `
     <div class="input-group mb-2">
-        <input type="text" class="form-control rounded" id="parameterKey${paramsConter}" placeholder="Enter the Key ${
-    paramsConter + 1
-  }">
-        <input type="text" class="form-control rounded" id="parameterValue${paramsConter}" placeholder="Enter the Value ${
-    paramsConter + 1
-  }">
-        <button type="button" class="btn btn-danger rounded delete-params-btn" title="Delete This param"><i class="fa fa-trash"></i></button>
+        <input type="text" class="form-control rounded param-key" id="key-params-value-${paramIndex}" placeholder="Enter the Key ${paramIndex}">
+        <input type="text" class="form-control rounded param-value" id="value-params-value-${paramIndex}" placeholder="Enter the Value ${paramIndex}">
+        <button class="btn btn-danger remove-params-btn rounded" id="remove-params-btn-${paramIndex}">
+            <i class="fa fa-trash"></i>
+        </button>
     </div>
-`;
-  let div = getElementFromString(string);
-  paramsCont.appendChild(div);
-  paramsConter = paramsConter + 1;
-
-  // deleting params
-  let delParamBtn = document.querySelectorAll(".delete-params-btn");
-
-  delParamBtn.forEach((btn, index) => {
-    btn.onclick = (e) => {
-      let test = document.getElementById(`key-params-${index}`);
-      e.target.parentElement.remove();
-    };
-  });
+  `;
+  const param = document.createElement("div");
+  param.classList.add("key-params");
+  param.innerHTML = paramEle;
+  paramsContainer.appendChild(param);
+  removeParam.push(grabEle(`#remove-params-btn-${paramIndex}`));
+  keyParam.push(grabEle(`#key-params-value-${headerIndex}`));
+  valueParam.push(grabEle(`#value-params-value-${headerIndex}`));
+  removeParam.forEach(
+    (btn) => (btn.onclick = (e) => e.target.parentElement.remove())
+  );
 };
 
-// postmonter from
-const postmonsterForm = document.querySelector(".postmonter-from");
-let url = document.querySelector(".url");
-
-let contentType;
-
-document.querySelector("#params-tab").onclick = (e) => {
-  contentType = "params";
+// adding header
+addHeaderBtn.onclick = (e) => {
+  headerIndex++;
+  const headerEle = `
+        <div class="input-group mb-2">
+            <input type="text" class="form-control rounded header-key" id="header-key-${headerIndex}" placeholder="Enter Header Key ${headerIndex}" />
+            <input type="text" class="form-control rounded header-value" id="header-value-${headerIndex}" placeholder="Enter Header Value ${headerIndex}" />
+            <button class="btn btn-danger remove-header-btn rounded" id="remove-params-btn-${headerIndex}">
+                <i class="fa fa-trash"></i>
+            </button>
+        </div>
+    </div>
+  `;
+  const header = document.createElement("div");
+  header.classList.add("header-params");
+  header.innerHTML = headerEle;
+  headerContainer.appendChild(header);
+  removeHeader.push(grabEle(`#remove-params-btn-${headerIndex}`));
+  headerKey.push(grabEle(`#header-key-${headerIndex}`));
+  headerValue.push(grabEle(`#header-value-${headerIndex}`));
+  removeHeader.forEach(
+    (btn) => (btn.onclick = (e) => e.target.parentElement.remove())
+  );
 };
-
-document.querySelector("#json-tab").onclick = (e) => {
-  contentType = "json";
-};
-
-postmonsterForm.onsubmit = (e) => {
+// form on submit
+form.onsubmit = (e) => {
   e.preventDefault();
-  let value = url.value;
-  // if (validateUrl(value) == false) showAlert("ERROR! Enter a valid URL.");
-  // if (value == "") showAlert("This feild can't be empty.");
-
-  let requestType;
-
-  let options = document.querySelectorAll("option");
-  options.forEach((option) => {
-    if (option.selected == true) {
-      requestType = option.value;
-    }
-  });
-
-  if (contentType == "params") {
-    let data = {};
-    for (let i = 0; i < paramsConter + 1; i++) {
-      if (document.getElementById("parameterKey" + (i + 1)) != undefined) {
-        let key = document.getElementById("parameterKey" + (i + 1)).value;
-        let value = document.getElementById("parameterValue" + (i + 1)).value;
-        data[key] = value;
+  let contentType = grabEle(".nav-link.active");
+  let contentTypeValue = contentType.innerText;
+  if (contentTypeValue == "Headers") {
+    grabAll(".header-key").forEach((item, i) => {
+      if (headerKey[i].value != "" && headerValue[i].value != "") {
+        headerData[i] = {
+          key: headerKey[i].value,
+          value: headerValue[i].value,
+        };
       }
-    }
-    data = JSON.stringify(data);
-  } else {
-    data = document.getElementById("requestJsonText").value;
+      // console.log(item.value);
+    });
+    console.log(headerData);
   }
 
-  if (contentType == "params") {
-    data = {};
-    for (let i = 0; i < addedParamCount + 1; i++) {
-      if (document.getElementById("parameterKey" + (i + 1)) != undefined) {
-        let key = document.getElementById("parameterKey" + (i + 1)).value;
-        let value = document.getElementById("parameterValue" + (i + 1)).value;
-        data[key] = value;
-      }
-    }
-    data = JSON.stringify(data);
-  } else {
-    data = document.getElementById("requestJsonText").value;
-  }
-
-  // if the request type is get, invoke fetch api to create a post request
-  if (requestType == "GET" && validateUrl(value) == true && value != "") {
-    responsePrism.innerText = "Fetching Data....";
-
-    fetch(value, {
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        responsePrism.innerText = JSON.stringify(data);
-        console.log(data);
-        Prism.highlightAll();
-      })
-      .catch((err) => {
-        showAlert(`Failed to fetch the ${value}`);
-        responsePrism.innerText = `Failed to fetch the ${value}`;
-        console.error(err);
-      });
-  } else {
-    fetch(value, {
-      method: "POST",
-      body: data,
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => response.text())
-      .then((data) => {
-        responsePrism.innerText = JSON.stringify(data);
-        console.log(data);
-        Prism.highlightAll();
-      })
-      .catch((err) => {
-        showAlert(`Failed to fetch the ${value}`);
-        responsePrism.innerText = `Failed to fetch the ${value}`;
-        console.error(err);
-      });
-  }
-
-  console.log(`Fetching ${value}`);
+  // if (validateUrl(url.value)) {
+  //   preview.innerText = "Fetching your response....";
+  //   let option = grabEle("option:checked");
+  //   let method = option.value;
+  //     if (method == "GET") {
+  //       fetch(url.value)
+  //         .then((res) => res.json())
+  //         .then((data) => {
+  //           let res = JSON.stringify(data);
+  //           console.log(contentTypeValue);
+  //           preview.innerText = res;
+  //           Prism.highlightAll();
+  //         })
+  //         .catch((err) => {});
+  //     }
+  //   } else {
+  //     showAlert("Your link is not valid.");
+  //     url.value = "";
+  // }
 };
