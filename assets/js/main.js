31937d15033a4452ced0b4c4a872a53a494f0bc4
Sahil Verma <sahilverma.wevdev@gmail.com>
2021-10-01 12:07:45 +0000

    Ready to deployment

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,30 +28,24 @@ let paramIndex = 1;
 let headerIndex = 1;
 let removeParam = [];
 let removeHeader = [];
-let keyParam = [];
-let valueParam = [];
-let headerKey = [grabEle(`#header-key-1`)];
-let headerValue = [grabEle(`#header-value-1`)];
-let headerData = [];
+let paramData = {};
+let headerData = {};
+
 // adding params
 addParamBtn.onclick = (e) => {
   paramIndex++;
   const paramEle = `
-    <div class="input-group mb-2">
-        <input type="text" class="form-control rounded param-key" id="key-params-value-${paramIndex}" placeholder="Enter the Key ${paramIndex}">
-        <input type="text" class="form-control rounded param-value" id="value-params-value-${paramIndex}" placeholder="Enter the Value ${paramIndex}">
-        <button class="btn btn-danger remove-params-btn rounded" id="remove-params-btn-${paramIndex}">
-            <i class="fa fa-trash"></i>
-        </button>
-    </div>
+      <input type="text" class="form-control rounded param-key" id="key-params-value-${paramIndex}" placeholder="Enter the Key ${paramIndex}">
+      <input type="text" class="form-control rounded param-value" id="value-params-value-${paramIndex}" placeholder="Enter the Value ${paramIndex}">
+      <button class="btn btn-danger remove-params-btn rounded" id="remove-params-btn-${paramIndex}">
+          <i class="fa fa-trash"></i>
+      </button>
   `;
   const param = document.createElement("div");
-  param.classList.add("key-params");
+  param.setAttribute("class", "key-params input-group mb-2");
   param.innerHTML = paramEle;
   paramsContainer.appendChild(param);
   removeParam.push(grabEle(`#remove-params-btn-${paramIndex}`));
-  keyParam.push(grabEle(`#key-params-value-${headerIndex}`));
-  valueParam.push(grabEle(`#value-params-value-${headerIndex}`));
   removeParam.forEach(
     (btn) => (btn.onclick = (e) => e.target.parentElement.remove())
   );
@@ -61,22 +55,17 @@ addParamBtn.onclick = (e) => {
 addHeaderBtn.onclick = (e) => {
   headerIndex++;
   const headerEle = `
-        <div class="input-group mb-2">
-            <input type="text" class="form-control rounded header-key" id="header-key-${headerIndex}" placeholder="Enter Header Key ${headerIndex}" />
-            <input type="text" class="form-control rounded header-value" id="header-value-${headerIndex}" placeholder="Enter Header Value ${headerIndex}" />
-            <button class="btn btn-danger remove-header-btn rounded" id="remove-params-btn-${headerIndex}">
-                <i class="fa fa-trash"></i>
-            </button>
-        </div>
-    </div>
+      <input type="text" class="form-control rounded header-key" id="header-key-${headerIndex}" placeholder="Enter Header Key ${headerIndex}" />
+      <input type="text" class="form-control rounded header-value" id="header-value-${headerIndex}" placeholder="Enter Header Value ${headerIndex}" />
+      <button class="btn btn-danger remove-header-btn rounded" id="remove-params-btn-${headerIndex}">
+          <i class="fa fa-trash"></i>
+      </button>
   `;
   const header = document.createElement("div");
-  header.classList.add("header-params");
+  header.setAttribute("class", "header-params input-group mb-2");
   header.innerHTML = headerEle;
   headerContainer.appendChild(header);
   removeHeader.push(grabEle(`#remove-params-btn-${headerIndex}`));
-  headerKey.push(grabEle(`#header-key-${headerIndex}`));
-  headerValue.push(grabEle(`#header-value-${headerIndex}`));
   removeHeader.forEach(
     (btn) => (btn.onclick = (e) => e.target.parentElement.remove())
   );
@@ -87,35 +76,58 @@ form.onsubmit = (e) => {
   let contentType = grabEle(".nav-link.active");
   let contentTypeValue = contentType.innerText;
   if (contentTypeValue == "Headers") {
-    grabAll(".header-key").forEach((item, i) => {
-      if (headerKey[i].value != "" && headerValue[i].value != "") {
-        headerData[i] = {
-          key: headerKey[i].value,
-          value: headerValue[i].value,
-        };
-      }
-      // console.log(item.value);
-    });
-    console.log(headerData);
+    headerData = [...grabAll(".header-params")].reduce((data, item) => {
+      const key = item.querySelector(".header-key").value;
+      const value = item.querySelector(".header-value").value;
+      if (key === "") return data;
+      return { ...data, [key]: value };
+    }, {});
+  }
+  if (contentTypeValue == "Params") {
+    paramData = [...grabAll(".key-params")].reduce((data, item) => {
+      const key = item.querySelector(".param-key").value;
+      const value = item.querySelector(".param-value").value;
+      if (key === "") return data;
+      return { ...data, [key]: value };
+    }, {});
   }
 
-  // if (validateUrl(url.value)) {
-  //   preview.innerText = "Fetching your response....";
-  //   let option = grabEle("option:checked");
-  //   let method = option.value;
-  //     if (method == "GET") {
-  //       fetch(url.value)
-  //         .then((res) => res.json())
-  //         .then((data) => {
-  //           let res = JSON.stringify(data);
-  //           console.log(contentTypeValue);
-  //           preview.innerText = res;
-  //           Prism.highlightAll();
-  //         })
-  //         .catch((err) => {});
-  //     }
-  //   } else {
-  //     showAlert("Your link is not valid.");
-  //     url.value = "";
-  // }
+  if (validateUrl(url.value)) {
+    preview.innerText = "Fetching your response....";
+    let option = grabEle("option:checked");
+    let method = option.value;
+    let json = grabEle("#requestJsonText");
+    let jsonData = json.value;
+    console.log(`Fetching ${url.value} wit ${method} method.`);
+    console.log({
+      url: url.value,
+      method,
+      params: paramData,
+      headers: headerData,
+      data: jsonData,
+    });
+    axios({
+      url: url.value,
+      method,
+      params: paramData,
+      headers: headerData,
+      data: jsonData,
+    })
+      .then((data) => {
+        let res = JSON.stringify(data);
+        console.log(data);
+        preview.innerText = res;
+        Prism.highlightAll();
+      })
+      .catch((e) => {
+        preview.innerText = `Can't fetch ${url.value}. Check console for more information.`;
+        showAlert(
+          `Can't fetch ${url.value}. Check console for more information.`
+        );
+        console.log(e);
+      });
+  } else {
+    showAlert("Your link is not valid.");
+    url.value = "";
+  }
 };
